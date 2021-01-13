db6b8478452d15fd69c40d72e659d6a565502931
SklifCybe <74813631+SklifCybe@users.noreply.github.com>
2021-01-13 20:19:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,6 @@ function countHollidaysInYear(holidays) {
     let count = 0;
     for (let i = 0; i < holidays.length; i++) {
         if (!checkWeekend(holidays[i])) {
-            debugger;
             count++;
         }
     }
@@ -56,26 +55,32 @@ function changeAnswer(holidays) {
     answer.innerHTML = 'Ответ: ' + countHollidaysInYear(holidays) + ` для ${holidays[0].getFullYear()} г.`;
 }
 
-function getHolidays() {
-    const holidays = [
-        new Date(getYear(), 0, 1),       // 1 января. Новый год
-        new Date(getYear(), 0, 7),       // 7 января. Православное рождество
-        new Date(getYear(), 2, 8),       // 8 марта. День женщин
-        new Date(getYear(), 3, 4),       // 4 апреля. Католическая пасха
-        new Date(getYear(), 4, 1),       // 1 мая. Праздник труда
-        new Date(getYear(), 4, 2),       // 2 мая. Православная пасха
-        new Date(getYear(), 4, 9),       // 9 мая. День победы
-        new Date(getYear(), 4, 11),      // 11 мая. Радуница
-        new Date(getYear(), 6, 3),       // 3 июля. День независимости РБ
-        new Date(getYear(), 10, 7),      // 7 ноября. День октябрьской революции
-        new Date(getYear(), 11, 25),     // 25 декабяр. Католическое рождество
-    ];
-    return holidays;
+function getRadonitsa(year) {
+    let a = (19 * (year % 19) + 15) % 30;
+    let b = ((2 * (year % 4) + 4 * (year % 7) + 6 * a + 6) % 7);
+    let p = 0;
+    if (a + b > 10)
+        p = new Date(year, 3, a + b - 9, 0, 0, 0, 0);
+    else
+        p = new Date(year, 2, 22 + a + b, 0, 0, 0, 0);
+    p.setDate(p.getDate() + 22);
+    return p;
+}
+
+function changeRadonitsaDay() {
+    let radonitsa = document.querySelector('#radonitsa');
+    let day = getRadonitsa(getYear()).getDate();
+    let month = getRadonitsa(getYear()).getMonth();
+    let nameMonth = 0;
+    if (month == 3) nameMonth = 'Апреля'
+    else nameMonth = 'Мая';
+
+    radonitsa.innerHTML = `${day} ${nameMonth}. `;
 }
 
 function backLight(holidays) {
     let holidaysHtml = document.querySelectorAll('.listHolidays');
-    let clearBackground = function() {
+    let clearBackground = function () {
         for (let i = 0; i < holidays.length; i++) {
             holidaysHtml[i].style.backgroundColor = 'white';
         }
@@ -85,6 +90,21 @@ function backLight(holidays) {
             holidaysHtml[i].style.backgroundColor = 'green';
         }
     }
+}
+
+function getHolidays() {
+    const holidays = [
+        new Date(getYear(), 0, 1),          // 1 января. Новый год
+        new Date(getYear(), 0, 7),          // 7 января. Православное рождество
+        new Date(getYear(), 2, 8),          // 8 марта. День женщин
+        new Date(getYear(), 4, 1),          // 1 мая. Праздник труда 
+        new Date(getYear(), 4, 9),          // 9 мая. День победы    
+        getRadonitsa(getYear()),            // Радуница
+        new Date(getYear(), 6, 3),          // 3 июля. День независимости РБ
+        new Date(getYear(), 10, 7),         // 7 ноября. День октябрьской революции
+        new Date(getYear(), 11, 25),        // 25 декабяр. Католическое рождество
+    ];
+    return holidays;
 }
 
 // Handlers
@@ -99,4 +119,5 @@ function start() {
     let holidays = getHolidays();
     checkYear(holidays);
     backLight(holidays);
+    changeRadonitsaDay();
 }
